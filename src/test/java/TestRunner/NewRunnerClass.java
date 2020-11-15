909569dcccd2b8926b0a2e87e963e1b846d55fbe
shivaram808 <shivaram.marella@gmail.com>
2020-11-15 18:27:49 +0530

    changes

--- src/test/java/TestRunner/NewRunnerClass.java
+++ src/test/java/TestRunner/NewRunnerClass.java
@@ -7,7 +7,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 //@CucumberOptions(features=".\\src\\test\\resources\\features",tags ="@MyAccount", glue = {"TestScripts"})
 
 @CucumberOptions(plugin = {"html:test-output/cucumberreports","json:test-output/cucumberreports/cucumber.json",
-"pretty:ttest-output/cucumberreports/cucumber-pretty.txt","junit:test-output/cucumberreports/cucumber-results.xml"},
+"pretty:test-output/cucumberreports/cucumber-pretty.txt","junit:test-output/cucumberreports/cucumber-results.xml"},
 features=".\\src\\test\\resources\\features",tags ="@AboutUS",glue = {"stepDefinition"})
 
 public class NewRunnerClass extends AbstractTestNGCucumberTests{
--- target/test-classes/features/EnquiryPage.feature
+++
@@ -1,9 +0,0 @@
-Feature: Automating Enquiry page 
-
-@Enquiry
-
-Scenario: Verify Enquiry page is displayed or not
-
-Given Login to TS RTC website using valid username and password
-Then click on Enquiry link
-And verify whether Enquiry page is displayed or not
